0e95421d3d2ba4a06e8d5c772c916caed1a8d916
danvu <danvuk11@gmail.com>
2025-02-13 21:27:20 -0500

    Clean up and fix bugs in EditorSystem

--- src/Engine/ComponentEditorSystem.hpp
+++ src/Engine/ComponentEditorSystem.hpp
@@ -28,40 +28,7 @@ public:
         _visualisers[visualiser->GetComponentRuntimeType()] = std::move(visualiser); 
     }
 
-    void Update() override
-    {
-        if (_inputReader->IsKeyPressed(Core::Key::P))
-        {
-            PuaseAllSystems(!_allSystemsPuased);
-        }
-
-        auto window = _uiSystem->OpenUIPanel("EngineProperties");
-
-        if (_uiSystem->BeginTabBar("EnginePropertiesTabBar"))
-        {
-            if (_uiSystem->BeginTabBarItem("Systems"))
-            {
-                _uiSystem->Text("Systems");
-                DrawSystemUI();
-                _uiSystem->EndTabBarItem();
-            }
-
-            if (_uiSystem->BeginTabBarItem("Scene"))
-            {
-                _uiSystem->Text("Scene");
-                DrawSceneUI();
-                _uiSystem->EndTabBarItem();
-            }
-
-            if (_uiSystem->BeginTabBarItem("Inspector"))
-            {
-                DrawInspectorUI();
-                _uiSystem->EndTabBarItem();
-            }
-
-            _uiSystem->EndTabBar();
-        }
-    }
+    void Update() override;
     
 private:
     std::unordered_map<std::type_index, std::shared_ptr<ComponentEditor>> _visualisers;
@@ -74,57 +41,7 @@ private:
 
     void DrawSystemUI();
     void DrawSceneUI();
-    void DrawInspectorUI()
-    {
-        auto& entities = _entityManager->GetAllEntities();
-
-        if (_selectedEntity >= entities.size())
-            _selectedEntity = entities.size() - 1;
-
-        std::vector<std::string> componentNames;
-        for (auto& entity : entities)
-        {
-            componentNames.push_back(entity->GetName());
-
-            Core::TransformComponent* transform = nullptr;
-            if (entity->QueryComponents(transform))
-            {
-                _uiSystem->AddWorldText(transform->Position, entity->GetName(), { 1.0f, 1.0f, 0.0f }, Render::Alignment::HorizontalCenter | Render::Alignment::VerticalCenter);
-            }
-        }
-        _uiSystem->Combo("Select Entity", &_selectedEntity, componentNames);
-
-        auto selectedEntity = entities[_selectedEntity];
-
-        char buffer[128];
-        strncpy_s(buffer, selectedEntity->GetName().c_str(), _TRUNCATE);
-        if (_uiSystem->Input("Name", buffer, 128))
-        {
-            selectedEntity->SetName(buffer);
-        }
-        _uiSystem->SameLine();
-        bool isActive = selectedEntity->IsActive();
-        if (_uiSystem->Checkbox("Is Active", &isActive))
-        {
-            selectedEntity->SetActive(isActive);
-        }
-        if (_uiSystem->Button("Destroy"))
-        {
-            selectedEntity->Destroy();
-            _selectedEntity--;
-        }
-
-        std::vector<Core::ECS::Component*> components;
-        selectedEntity->ListAttachedComponents(components);
-        for (auto& component : components)
-        {
-            auto visualiser = _visualisers.find(typeid(*component));
-            if (visualiser != _visualisers.end())
-            {
-                visualiser->second->DrawUI(component);
-            }
-        }
-    }
+    void DrawInspectorUI();
 
     void PuaseAllSystems(bool pause);
 };
