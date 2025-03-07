f9342704d24238c4b9cdc9ae87ce13853a8f58f7
danvu <danvuk11@gmail.com>
2025-03-07 15:16:38 -0500

    Implement Gizmos

---
+++ src/RenderOpenGL/GizmoManager.cpp
@@ -0,0 +1,82 @@
+//
+// Created by danvu on 2/23/2025.
+//
+
+#include "GizmoManager.hpp"
+
+#include "glad/gl.h"
+#include "OpenGLRenderSystem.hpp"
+
+namespace Snowglobe::RenderOpenGL {
+
+void GizmoVertexLayoutDescriptor::SetupAtributePointers() const
+{
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GizmoVertex), static_cast<void*>(offsetof(GizmoVertex, position)));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(GizmoVertex), reinterpret_cast<void*>(offsetof(GizmoVertex, color)));
+    glEnableVertexAttribArray(1);
+}
+
+void GizmoManager::Init()
+{
+    auto shaderCompiler = OpenGLRenderSystem::GetInstance()->GetShaderCompiler();
+    auto vertexShader = shaderCompiler->GetOrCompileShader(Core::SnowFileHandle("gizmo.vert"));
+    auto fragmentShader = shaderCompiler->GetOrCompileShader(Core::SnowFileHandle("gizmo.frag"));
+
+    PipelineSetupParams params = {vertexShader, fragmentShader};
+    _programID = shaderCompiler->GetOrCratePipeline(params);
+
+    glGenBuffers(1, &_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+
+    _bufferByteSize = GizmoVertexLayoutDescriptor::GetInstance()->GetStride() * BATCH_SIZE;
+
+    glBufferData(GL_ARRAY_BUFFER, _bufferByteSize * 2, nullptr, GL_STATIC_DRAW); // both filled and wire geometry
+    glBufferSubData(GL_ARRAY_BUFFER, 0, _bufferByteSize, _vertices.data());
+    glBufferSubData(GL_ARRAY_BUFFER, _bufferByteSize, _bufferByteSize, _verticesWire.data());
+
+    glGenVertexArrays(1, &_vao);
+    glBindVertexArray(_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+    GizmoVertexLayoutDescriptor::GetInstance()->SetupAtributePointers();
+    glBindVertexArray(0);
+
+    CreateDiskVertices();
+    CreateDiskVerticesWire();
+}
+void GizmoManager::Draw()
+{
+    bool needsDraw = _vertexCount > 0 || _vertexWireCount > 0;
+    if (!needsDraw)
+        return;
+
+    Flush();
+
+    glUseProgram(_programID);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    _sceneParameters.Bind(OpenGLRenderSystem::GetInstance()->GetCamera(), _programID);
+    glBindVertexArray(_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+    if (_vertexCount > 0)
+        glDrawArrays(GL_TRIANGLES, 0, _vertexCount);
+    if (_vertexWireCount > 0)
+        glDrawArrays(GL_LINES, BATCH_SIZE, _vertexWireCount);
+
+    glEnable(GL_DEPTH_TEST);
+    //End
+    _vertexCount = 0;
+    _vertexWireCount = 0;
+}
+void GizmoManager::Flush()
+{
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+    if (_vertexCount > 0)
+        glBufferSubData(GL_ARRAY_BUFFER, 0, GizmoVertexLayoutDescriptor::GetInstance()->GetStride() * _vertexCount, static_cast<const void*>(_vertices.data()));
+
+    if (_vertexWireCount > 0)
+        glBufferSubData(GL_ARRAY_BUFFER, _bufferByteSize, GizmoVertexLayoutDescriptor::GetInstance()->GetStride() * _vertexWireCount, static_cast<const void*>(_verticesWire.data()));
+}
+
+} // namespace Snowglobe::RenderOpenGL
---
+++ src/RenderOpenGL/Shaders/gizmo.vert
@@ -0,0 +1,21 @@
+#version 330 core
+
+struct SceneParameters
+{
+    mat4 viewProjection;
+    vec3 viewPosition;
+};
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColor;
+
+uniform SceneParameters sceneParameters;
+
+out vec4 VertexColor;
+
+void main()
+{
+    // gl_Position = sceneParameters.projection * sceneParameters.view * rentity.model * vec4(aPos, 1.0);
+    gl_Position = sceneParameters.viewProjection * vec4(aPos, 1.0);
+    VertexColor = aColor;
+}
